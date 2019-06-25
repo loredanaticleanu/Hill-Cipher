24c95a8741bd9f73d8e6e5758ae87e6e2ff85748
Eduard <georgescu.eduard@outlook.com>
2019-06-25 11:59:14 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -18,9 +18,11 @@ int find_det(int matrice_initiala[N][N], int n, int det) {
 	}
 	return det;
 }
+//asasfdasfasfasf
 //ceva
 //asta este dor pentru test
 //acum o sa tetez un pull request pentru acest cod si o sa ii dau merge cu master
+
 int cmmdc(int a, int b) {
 	while (a != b) {
 		if (a > b) {
